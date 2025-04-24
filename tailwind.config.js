47b95df40bfd9f9515a7cbe16b41d7b9a2807e46
Nemanja Spasic <59572186+nspa4921@users.noreply.github.com>
2025-04-24 16:00:21 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Views/**/*.cshtml',
+    './**/*.cshtml',
+    './**/*.razor', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
